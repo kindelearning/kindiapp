16e2e8dc5523738f2d6e48921b7796b103b9c565
Kindi Lerning <KINDI.learn@gmail.com>
2024-10-27 15:28:26 +0530

    Resuming Flow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.kindilearning.app",
-  appName: "kindiapp",
+  appName: "kindi Learning",
   webDir: "out",
   bundledWebRuntime: false,
 };

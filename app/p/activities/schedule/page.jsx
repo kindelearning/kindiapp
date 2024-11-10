7d5d91539ae0cfee3fb29fedc3b118a05014e466
Kindi Lerning <KINDI.learn@gmail.com>
2024-11-10 16:36:58 +0530

    updated the schedular data with Hygraph Content

--- app/p/activities/schedule/page.jsx
+++ app/p/activities/schedule/page.jsx
@@ -1,60 +1,7 @@
 import { NewCalendar } from "@/app/Widgets";
-
-// /**
-//  * @Main_account
-//  */
-// const HYGRAPH_ENDPOINT =
-//   "https://ap-south-1.cdn.hygraph.com/content/cm1dom1hh03y107uwwxrutpmz/master";
-// const HYGRAPH_TOKEN =
-//   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE3MjcwNjQxNzcsImF1ZCI6WyJodHRwczovL2FwaS1hcC1zb3V0aC0xLmh5Z3JhcGguY29tL3YyL2NtMWRvbTFoaDAzeTEwN3V3d3hydXRwbXovbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQtYXAtc291dGgtMS5oeWdyYXBoLmNvbS8iLCJzdWIiOiI2Yzg4NjI5YS1jMmU5LTQyYjctYmJjOC04OTI2YmJlN2YyNDkiLCJqdGkiOiJjbTFlaGYzdzYwcmZuMDdwaWdwcmpieXhyIn0.YMoI_XTrCZI-C7v_FX-oKL5VVtx95tPmOFReCdUcP50nIpE3tTjUtYdApDqSRPegOQai6wbyT0H8UbTTUYsZUnBbvaMd-Io3ru3dqT1WdIJMhSx6007fl_aD6gQcxb-gHxODfz5LmJdwZbdaaNnyKIPVQsOEb-uVHiDJP3Zag2Ec2opK-SkPKKWq-gfDv5JIZxwE_8x7kwhCrfQxCZyUHvIHrJb9VBPrCIq1XE-suyA03bGfh8_5PuCfKCAof7TbH1dtvaKjUuYY1Gd54uRgp8ELZTf13i073I9ZFRUU3PVjUKEOUoCdzNLksKc-mc-MF8tgLxSQ946AfwleAVkFCXduIAO7ASaWU3coX7CsXmZLGRT_a82wOORD8zihfJa4LG8bB-FKm2LVIu_QfqIHJKq-ytuycpeKMV_MTvsbsWeikH0tGPQxvAA902mMrYJr9wohOw0gru7mg_U6tLOwG2smcwuXBPnpty0oGuGwXWt_D6ryLwdNubLJpIWV0dOWF8N5D6VubNytNZlIbyFQKnGcPDw6hGRLMw2B7-1V2RpR6F3RibLFJf9GekI60UYdsXthAFE6Xzrlw03Gv5BOKImBoDPyMr0DCzneyAj9KDq4cbNNcihbHl1iA6lUCTNY3vkCBXmyujXZEcLu_Q0gvrAW3OvZMHeHY__CtXN6JFA";
-
-// const client = new GraphQLClient(HYGRAPH_ENDPOINT, {
-//   headers: {
-//     Authorization: `Bearer ${HYGRAPH_TOKEN}`,
-//   },
-// });
-
-// const GET_ACCOUNT_BY_EMAIL = gql`
-//   query GetAccountByEmail($email: String!) {
-//     account(where: { email: $email }) {
-//       name
-//       username
-//       email
-//       profilePicture {
-//         url
-//       }
-//       isVerified
-//     }
-//   }
-// `;
+import TestingCalendar from "@/app/Widgets/Schedular/TestingCalendar";
 
 export default function Schedule() {
-  // const { data: session, status } = useSession();
-  // const [profileData, setProfileData] = useState(null);
-
-  // const fetchUserData = async (email) => {
-  //   try {
-  //     const data = await client.request(GET_ACCOUNT_BY_EMAIL, { email });
-  //     console.log("User data", data);
-  //     setProfileData(data.account);
-  //   } catch (error) {
-  //     console.error("Error fetching profile data:", error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (session && session.user) {
-  //     fetchUserData(session.user.email);
-  //   }
-  // }, [session]);
-
-  // if (status === "loading") {
-  //   return (
-  //     <div className="w-full h-screen flex justify-center items-center">
-  //       <Loading />
-  //     </div>
-  //   );
-  // }
   return (
     <>
       <section className="w-full h-auto bg-[#EAEAF5] pb-24 items-center justify-center py-4 flex flex-col md:flex-row gap-[20px]">
@@ -78,6 +25,7 @@ export default function Schedule() {
           </div>
           <div className="claracontainer md:p-0 p-0 py-4 w-full flex lg:hidden flex-col overflow-hidden gap-8">
             <NewCalendar />
+            {/* <TestingCalendar /> */}
           </div>
         </div>
       </section>

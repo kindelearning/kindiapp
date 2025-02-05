612c336d9bf97683740233ee07f388d1be1b806e
Kindi Learning <KINDI.learn@gmail.com>
2025-02-05 11:19:13 +0530

    updeated

--- app/p/activities/ActivityDetailClient.jsx
+++ app/p/activities/ActivityDetailClient.jsx
@@ -526,21 +526,33 @@ export function ActivityDetailClient2({ params }) {
     </>
   );
 }
+export async function fetchActivityByDocumentId(documentId) {
+  const res = await fetch(
+    `https://lionfish-app-98urn.ondigitalocean.app/api/activities/${documentId}?populate=*`
+  );
+  const data = await res.json();
+  if (!data || !data.data) {
+    return null; // If data is not found
+  }
+
+  return data.data;
+}
+
 export default function ActivityDetailClient({ params }) {
   const { id } = params;
   const [activity, setActivity] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  
+  
   useEffect(() => {
-    if (!id) return; // Avoid fetching if id is undefined/null
+    // if (!id) return; // Avoid fetching if id is undefined/null
 
     const fetchActivity = async () => {
       try {
         setLoading(true);
 
-        const response = await fetch(
-          `https://lionfish-app-98urn.ondigitalocean.app/api/activities/${id}?populate=*`
-        );
+        const response = await fetchActivityByDocumentId(id)
 
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
@@ -562,6 +574,8 @@ export default function ActivityDetailClient({ params }) {
 
     fetchActivity();
   }, [id]);
+ 
+ 
   console.log("Activity Data", activity);
   if (loading) return <p>Loading...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
@@ -571,28 +585,21 @@ export default function ActivityDetailClient({ params }) {
 
   const {
     Title,
-    Skills,
-    LearningAreaIcons,
-    Theme,
-    FocusAge,
-    ActivityDate,
-    SetUpTime,
-    Gallery,
-    Accordions,
+   
   } = activity;
 
   return (
     <>
       <NewHeader headerText="Activity" />
-
-      <section className="w-full h-auto -my-[16px] bg-[#EAEAF5] items-center justify-center py-0 px-0 flex flex-col md:flex-row gap-[20px]">
+      <div className="w-full hidden text-[#3f3a64] claraheading capitalize">
+            {Title}
+          </div>
+      {/* <section className="w-full h-auto -my-[16px] bg-[#EAEAF5] items-center justify-center py-0 px-0 flex flex-col md:flex-row gap-[20px]">
         <div className="claracontainer p-0 lg:p-8 xl:p-12 w-full flex flex-col md:flex-row overflow-hidden gap-8">
           <div className="w-full hidden text-[#3f3a64] claraheading capitalize">
             {Title}
           </div>
-          {/* Col 1(R1) */}
           <div className="claracontainer lg:min-w-[60%] lg:w-full bg-[#ffffff] md:bg-[#ffffff] pb-4 lg:bg-[#eaeaf5] py-0 flex flex-col justify-start items-start gap-4">
-            {/* Row 1(C1) */}
             <div className="claracontainer py-0 flex flex-col justify-between items-start gap-8">
               {Gallery ? (
                 <ProductMedia gallery={Gallery} />
@@ -606,7 +613,6 @@ export default function ActivityDetailClient({ params }) {
                 </div>
               )}
             </div>
-            {/* Row 1(R2) */}
             <div className="claracontainer lg:hidden w-full flex flex-col px-4 lg:px-0 justify-start items-start gap-4">
               <div className="flex w-full flex-col justify-normal items-center gap-2">
                 <div className="text-[#0a1932]  text-start justify-start items-start w-full font-fredoka font-semibold text-[24px] md:text-[28px] lg:text-[28px]">
@@ -680,7 +686,6 @@ export default function ActivityDetailClient({ params }) {
                 </div>
               </div>
             </div>
-            {/* Row - 2(C1) */}
             <div className="items-center px-4 hidden lg:px-0 w-full lg:min-w-[600px] justify-center lg:flex flex-col gap-2">
               <div className="px-4 mb-6 md:hidden flex w-full py-6 bg-white rounded-xl shadow gap-3 flex-col justify-center items-center">
                 <div className="text-[#3f3a64] text-base font-semibold font-montserrat uppercase leading-[19px]">
@@ -735,14 +740,11 @@ export default function ActivityDetailClient({ params }) {
             </div>
           </div>
 
-          {/* Col Two */}
           <div className="claracontainer p-0 pb-24 flex flex-col justify-start items-start gap-8">
-            {/* Row 1(R2) */}
             <div className="claracontainer hidden lg:flex w-full flex-col px-4 lg:px-0 justify-start items-start gap-4">
               <div className="flex w-full flex-col justify-normal items-center gap-2">
                 <div className="text-[#0a1932] text-start justify-start items-start w-full font-fredoka font-semibold text-[24px] md:text-[28px] lg:text-[28px]">
                   {Title}
-                  {/* | {activity.id} | */}
                 </div>
                 <div className="items-center w-full justify-center flex flex-col gap-2">
                   <ActivityAttribute
@@ -865,7 +867,6 @@ export default function ActivityDetailClient({ params }) {
               ))}
             </div>
 
-            {/* Row 2(R2) */}
             <div className="flex w-full flex-col py-6 md:py-0 justify-start items-start gap-2">
               <div className="w-full md:flex hidden justify-between items-center p-6 bg-white rounded-xl shadow">
                 <ChevronLeft />
@@ -941,18 +942,17 @@ export default function ActivityDetailClient({ params }) {
                 <div className="text-[#3f3a64] text-base font-semibold font-montserrat uppercase leading-[19px]">
                   Mark Activity as Complete{" "}
                 </div>
-                {/* <MarkActivityCompleteForm passactivityId={matchedActivityId} /> */}
+                <MarkActivityCompleteForm passactivityId={matchedActivityId} />
               </div>
             </div>
           </div>
 
-          {/* Mobile Specific Row */}
           <div className="flex md:hidden max-w-full overflow-hidden z-50 shadow-upper pt-2 pb-4 px-2 mb-[72px] rounded-t-[8px] justify-between items-center gap-1 bg-[white] shadow-sm fixed bottom-0 left-0 w-full">
             <PrintDocument activityid={activity.documentId} />
-            {/* <MarkActivityCompleteForm passactivityId={matchedActivityId} /> */}
+            <MarkActivityCompleteForm passactivityId={matchedActivityId} />
           </div>
         </div>
-      </section>
+      </section> */}
     </>
   );
 }

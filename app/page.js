295324f78dca96d68704cab227123ac419e3fc4a
Kindi Learning <KINDI.learn@gmail.com>
2025-02-07 11:29:35 +0530

    Now Working on Schedular

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <section className="w-full flex flex-col gap-0 justify-center items-center">
       <LocalHeader />
-      <div className="w-full -mt-[8px]  rounded-t-[12px] pb-12 flex flex-col overflow-hidden gap-0">
+      <div className="w-full -mt-[8px] z-50  rounded-t-[12px] pb-12 flex flex-col overflow-hidden gap-0">
         <MobileHero />
         <MobileThemes />
         <MobileActivity />
